a27afe9939356ed6517ad333ca9bd0e0ab995636
ajazsidhiq <ajazsidhiq97@gmail.com>
2019-02-03 03:19:42 +0530

    testing

--- careers/ajaxjs.php
+++ careers/ajaxjs.php
@@ -1,5 +1,6 @@
 <?php
 // Fetching Values From URL
+echo "check1";
 $name = $_POST['nameInput'];
 $email = $_POST['emailInput'];
 $contact = $_POST['contactInput'];
@@ -8,10 +9,10 @@ $is_fresher = $_POST['fresh'];
 $address = $_POST['addressInput'];
 $cvLink = $_POST['cvLinkInput'];
 
-
+echo "check2";
 $connection = mysql_connect("localhost", "qlrsabna_abconsortium", "Abconsortium"); // Establishing Connection with Server..
 $db = mysql_select_db("qlrsabna_abconsortium", $connection); // Selecting Database
-echo "insert into job_applications (name, email) values ('$name', '$email')";
+echo "check3";
 if (isset($_POST['name'])) {
 $query = mysql_query("insert into job_applications (name, email) values ('$name', '$email')"); //Insert Query
 echo "Form Submitted succesfully";
